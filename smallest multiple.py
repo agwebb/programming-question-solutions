aba3bca5c823969cde0b3723506390b818c62bb8
Webb <165248580+agwebb@users.noreply.github.com>
2024-12-17 21:52:55 -0800

    updated it to reduce the run time by n-1

--- smallest multiple.py
+++ smallest multiple.py
@@ -1,14 +1,18 @@
+import time
 class hopes_and_prayers:
     def __init__(self, limiter):
         self.limiter = limiter
 
     def logic(self):
-        checked = 1
+        checked = self.limiter
+        print(self.limiter)
         while True:
             if all(checked % i == 0 for i in range(1, self.limiter + 1)):
                 return checked
-            checked += 1
-
+            print(checked, "\n")
+            #checked += indexer
+            checked+= self.limiter
+            #hi
 instance1 = hopes_and_prayers(20)
 
-print(instance1.logic())
+print("result: ", instance1.logic())
